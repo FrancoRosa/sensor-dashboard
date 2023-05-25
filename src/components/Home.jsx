4f410ebb996fce0c80ba8705bcf44f91018bab04
FrancoRosa <km115.franco@gmail.com>
2023-05-24 23:33:45 -0500

    Add subscription to changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,23 @@ import NewNode from "./NewNode";
 import Nodes from "./Nodes";
 // import NodeDetails from "./NodeDetails";
 import MainLabel from "./MainLabel";
+import { supabase } from "../js/supabase";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const devices = supabase
+      .channel("custom-update-channel")
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "devices" },
+        (payload) => {
+          console.log("Change received!", payload);
+        }
+      )
+      .subscribe();
+  }, []);
+
   return (
     <div className="container" style={{ width: "100vw" }}>
       <MainLabel />
