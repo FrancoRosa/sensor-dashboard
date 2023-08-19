faa4cf170ff9a6b1ab76b733eba04a0ac2a9bf7c
FrancoRosa <km115.franco@gmail.com>
2023-08-19 11:25:59 -0500

    Add payload based clasification

--- api/supabase.js
+++ api/supabase.js
@@ -4,21 +4,46 @@ const { url, anon } = require("./credentials");
 const supabase = createClient(url, anon);
 let particles = {};
 const positions = {
-  particle: 1,
-  timestamp: 2,
-  lat: 3,
-  lng: 4,
-  pm1: 5,
-  pm25: 6,
-  pm4: 100,
-  pm10: 7,
-  temp: 35,
-  hum: 36,
-  gas_op1_w: 42,
-  gas_op1_r: 43,
-  gas_op2_w: 44,
-  gas_op2_r: 45,
-  noise: 46,
+  47: {
+    particle: 1,
+    timestamp: 2,
+    lat: 3,
+    lng: 4,
+    pm1: 5,
+    pm25: 6,
+    pm4: 100,
+    pm10: 7,
+    temp: 35,
+    hum: 36,
+    gas_op1_w: 42,
+    gas_op1_r: 43,
+    gas_op2_w: 44,
+    gas_op2_r: 45,
+    noise: 46,
+  },
+  21: {
+    particle: 0,
+    timestamp: 1,
+    lat: 2,
+    lng: 3,
+    pm1: 4,
+    pm25: 5,
+    pm4: 6,
+    pm10: 7,
+    pm05num: 8,
+    pm1num: 9,
+    pm25num: 10,
+    pm4num: 11,
+    pm10num: 12,
+    part_size: 13,
+    temp: 14,
+    hum: 15,
+    gas_op1_w: 16,
+    gas_op1_r: 17,
+    gas_op2_w: 18,
+    gas_op2_r: 19,
+    noise: 46,
+  },
 };
 
 const validate = (value) => {
@@ -30,24 +55,28 @@ const getParticles = async () => {
 };
 
 const payloadToObject = (payload) => {
-  // sample data: "0,340030000d47373432363837,1684867032,49.957066,82.592056,1.30,2.59,7.50,3936,351,47,19,31,19,11,7,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,800,622,624,17.90,36.33,1,998.00,998.00,na,na,na,na,na,na,na"
+  // sample data:
+  // 0,340030000d47373432363837,1684867032,49.957066,82.592056,1.30,2.59,7.50,3936,351,47,19,31,19,11,7,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,800,622,624,17.90,36.33,1,998.00,998.00,na,na,na,na,na,na,na
+  // e00fce68c9677c2fe1029d34,946598400,0.000000,0.000000,3.21,3.40,3.41,3.41,0.00,0.00,0.00,0.00,0.00,0.00,25.6,49.8,462,420,330,300,4085
   const values = payload.split(",");
+  const len = values.length;
+  console.log(len);
   const result = {
-    id: particles[values[positions.particle]],
+    id: particles[values[positions[len].particle]],
     data: {
-      lat: values[positions.lat],
-      lng: values[positions.lng],
-      pm1: values[positions.pm1],
-      "pm2.5": values[positions.pm25],
-      pm10: values[positions.pm10],
-      temp: values[positions.temp],
-      hum: values[positions.hum],
-      gas_op1_w: validate(values[positions.gas_op1_w]),
-      gas_op1_r: validate(values[positions.gas_op1_r]),
-      gas_op2_w: validate(values[positions.gas_op2_w]),
-      gas_op2_r: validate(values[positions.gas_op2_r]),
-      noise: validate(values[positions.noise]),
-      updated_at: new Date(parseInt(values[positions.timestamp]) * 1000),
+      lat: values[positions[len].lat],
+      lng: values[positions[len].lng],
+      pm1: values[positions[len].pm1],
+      "pm2.5": values[positions[len].pm25],
+      pm10: values[positions[len].pm10],
+      temp: values[positions[len].temp],
+      hum: values[positions[len].hum],
+      gas_op1_w: validate(values[positions[len].gas_op1_w]),
+      gas_op1_r: validate(values[positions[len].gas_op1_r]),
+      gas_op2_w: validate(values[positions[len].gas_op2_w]),
+      gas_op2_r: validate(values[positions[len].gas_op2_r]),
+      noise: validate(values[positions[len].noise]),
+      updated_at: new Date(parseInt(values[positions[len].timestamp]) * 1000),
     },
   };
   console.log(result);
